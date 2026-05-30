37c7a742070f63d50be252d379a955b4d4d0ed1e
伍小拾 <96697599+YvonneWXS@users.noreply.github.com>
2026-05-30 17:10:41 +0800

    chore: save current MoG state before Fix 1 implementation

--- model/part1_GAT/RadioGAT-Multi-band-Radiomap-Reconstruction/run_serial.py
+++ model/part1_GAT/RadioGAT-Multi-band-Radiomap-Reconstruction/run_serial.py
@@ -13,10 +13,10 @@ PYTHON = r"D:\1_developTool\4_conda\envs\smartLoc\python.exe"
 SRC_DIR = r"D:\3_document\4_research\NLOS Signal Identification and Correction\model\part1_GAT\RadioGAT-Multi-band-Radiomap-Reconstruction"
 
 EXPERIMENTS = [
-    ("berlin1_potsdamer_platz", "exp_016"),
-    ("berlin2_gendarmenmarkt", "exp_017"),
-    ("frankfurt1_maintower", "exp_018"),
-    ("frankfurt2_westendtower", "exp_019"),
+    ("berlin1_potsdamer_platz", "exp_021"),
+    ("berlin2_gendarmenmarkt", "exp_022"),
+    ("frankfurt1_maintower", "exp_023"),
+    ("frankfurt2_westendtower", "exp_024"),
 ]
 
 
